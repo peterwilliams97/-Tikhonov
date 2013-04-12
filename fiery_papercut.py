304c6eae44b41b33140505678e05046228b2c17e
Peter Williams <peter.williams.97@gmail.com>
2013-04-12 12:46:29 +1000

    New job history tracking and conversions based on Ajay's answers

--- fiery_papercut.py
+++ fiery_papercut.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+
     Script to test Fiery cost accounting API working with PaperCut.
 
     Assumes access to a Fiery and a PaperCut server.
@@ -11,8 +12,6 @@
 
     If you do this then please set the admin password to "password" or 
     change DEFAULT_PAPERCUT_PWD to the PaperCut admin password.
-    
-    You should run this script on the computer the PaperCut server is running on.
 
     Viewing Fiery jobs in PaperCut
     -------------------------------
@@ -27,18 +26,16 @@
         There is more information and screen shots for this in  
         https://docs.google.com/a/papercut.com/document/d/1fj4v6kFj5GHI_pbHiO1sie_bysQZw5z6jv15YW_5fCg
 
-
+    Notes:
+    ------
     DEFAULT_* contain all default states
     Update DEFAULT_* to reflect your environment or vice-versa
 
     TODO:
         Handle multiple Fierys
-        Figure out how to request only jobs printed on Fiery since last request
         Scan through all jobs on Fiery (see fy_reqStart, fy_reqCount) 
         Enable page level color detection for Fiery printer
         Remove exit()s in production code
-        Add other more Fiery job attributes to PaperCut jobs
-        Fix PaperCut server\printer name for Fierys
 """
 from __future__ import division
 
@@ -48,6 +45,7 @@ import re
 import sys
 import optparse
 import time
+import datetime
 import xmlrpclib
 import pprint
 import cPickle as pickle
@@ -73,7 +71,10 @@ def load_object(path, default=None):
     except:
         return default  
 
-
+def first_non_empty(iterable):
+    """Return first non-empty element in iterable"""
+    return next(s for s in iterable if s)   
+    
 #
 # Fiery code
 #        
@@ -82,7 +83,6 @@ fy_reqStart = 0
 fy_reqCount = 100
 
 def fiery_load_api_key(key_file):   
-    """Load Fiery API key from key_file and return it."""
     try:   
         return open(key_file, 'rb').read()
     except Exception, e:
@@ -91,6 +91,10 @@ def fiery_load_api_key(key_file):
 
 
 def fiery_login(api_key, fiery, username, password, verbose=False): 
+    """Login to Fiery and return a URL and session cookie
+        TODO: 
+            Don't exit()
+    """
 
     auth = {
         'username': username, 
@@ -98,15 +102,13 @@ def fiery_login(api_key, fiery, username, password, verbose=False):
         'accessrights': {'a1': api_key}
     }
     if verbose:
-        print json.dumps(auth)
+        print('fiery_login: fiery=%s,auth=%s'  
+               % (fiery, json.dumps(auth)))
 
     if not fiery:
         print('error: no server specified')
         exit(2)
-
-    if verbose:
-        print('connecting to ' + fiery + '...')
-
+    
     url = 'https://%s/live' % fiery
     
     r = requests.post('https://%s/live/login' % fiery, 
@@ -136,24 +138,24 @@ def fiery_login(api_key, fiery, username, password, verbose=False):
 
     return url, sessionCookie    
 
-def fiery_fetch_jobs(url, sessionCookie, verbose=False): 
+def fiery_fetch_jobs(url, sessionCookie, start_id, count, verbose=False): 
 
-    # Request Fiery job log
-    if verbose:
-        print("retrieving...");
+    # Request job log
+
     headers = {'Cookie': '_session_id=%s;' % sessionCookie}
-    full_url = '%s/api/v1/cost?start_id=%d&count=%d' % (url, fy_reqStart, fy_reqCount)
+    full_url = '%s/api/v1/cost?start_id=%d&count=%d' % (url, start_id, count)
+
+    if verbose:
+        print('Retrieving Fiery jobs: url="%s"' % full_url)
+
     r = requests.get(full_url, headers=headers, verify=False)
     if r.status_code != 200:
         print('error: url=%s, http code=%d' % (full_url, r.status_code))
         exit(9)
 
-    # Print the list of printed jobs
-    prnJobs = json.loads(r.text)
-    if verbose:
-        print(str(len(prnJobs)) + ' entries retrieved')
+    # The list of printed jobs
+    return json.loads(r.text)
 
-    return prnJobs
 
 #
 # PaperCut code
@@ -161,7 +163,7 @@ def fiery_fetch_jobs(url, sessionCookie, verbose=False):
 
 def papercut_init(host_name, port, auth_token, account_name):
     """Standard PaperCut XMLRPC initialization
-        Instantiate a server and create a shared account if it doesn't 
+        Instantiate a server and create the accout if it doesn't 
         already exist.
         
         host_name: Network name/IP address of PaperCut server
@@ -171,7 +173,7 @@ def papercut_init(host_name, port, auth_token, account_name):
         Returns: Instantiated XMLRPC server
     """
 
-    print('Connecting to PaperCut host %s:%d' % (host_name, port))
+    print('Connecting to PaperCut "%s:%d"' % (host_name, port))
 
     server = xmlrpclib.Server('http://%s:%d/rpc/api/xmlrpc' % (host_name, port))
 
@@ -203,64 +205,69 @@ def papercut_log_jobs(server, auth_token, pc_jobs):
 
 #
 # Job conversion/manipulation code
-#    
+#
+def convert_boolean(fy_bool_str):
+    if fy_bool_str:
+        if fy_bool_str[0].lower() == 'y':
+            return 'TRUE'
+        if fy_bool_str[0].lower() == 'n':
+            return 'FALSE'
+    return '???'  
+    
+def convert_time(fy_time):
+    """Convert a Fiery date-time to a PaperCut date-time
+
+        fy_time: Fiery date-time
+        Returns: PaperCut date-time corresponding to fy_time if fy_time 
+            is parsed successfully or an obviously bogus date-time if
+            it is not.
+
+        TODO: 
+            Is Fiery date-time GMT or local?
+            Is Fiery date-time string localized or always English?
+    """ 
+    try:
+        tm = time.strptime(fy_time, '%H:%M %b %d, %Y')  
+        dt = datetime.datetime(*tm[:6])
+        return dt.isoformat().replace(':', '').replace('-', '')
+    except e:
+        print('convert_time: Invalid fy_time="%s": e' % (fy_time, e))
+        return '1111-11-11T11:11:11'
+
+
   
 FIERY_PAPERCUT_MAP = {
+    'printer': lambda job: job['fiery'],
+    'user': lambda job: first_non_empty([job.get('username', None), 
+                            job.get('authuser', None), 'blank on Fiery']),
     'comment': lambda job: 'Fiery id: %s' % job['id'],
-    'user': lambda job: job['username'] if job['username'] else 'blank on Fiery',
-    'server': lambda job: job['fiery'],
-    'printer': lambda job: 'print',
+    'time': lambda job: convert_time(job['date']),
+    'document-name': lambda job: job['title'],
+    'document-size-kb': lambda job: int(job['size'])//1024,
+    'paper-size-name': lambda job: job['media size'],
     'copies': lambda job: int(job['copies printed']),
     'total-pages': lambda job: (int(job['total blank pages printed'])
                               + int(job['total bw pages printed'])
-                              + int(job['total color pages printed'])),                              
-    'total-color-pages': lambda job: int(job['total color pages printed'])
+                              + int(job['total color pages printed'])),
+    'total-color-pages': lambda job: int(job['total color pages printed']),
+    'duplex': lambda job: convert_boolean(job['duplex printed']),
 }
    
-def job_fiery_to_papercut(fy_job, pc_account_name):
+def convert_job(fy_job, pc_server, pc_account_name):
     """Convert a Fiery job to a PaperCut job
 
         fy_job: Fiery job as a dict
         pc_account_name: PaperCut account name
         Returns: PaperCut job corresponding to fy_job
 
-        TODO: 
-            Check this conversion with Fiery team
-            Add remaining attributes
+        TODO: Check this conversion with Fiery team
     """    
     pc_job = dict((k, FIERY_PAPERCUT_MAP[k](fy_job)) for k in FIERY_PAPERCUT_MAP) 
+    pc_job['server'] = pc_server
     pc_job['shared-account'] = pc_account_name
+    pc_job['grayscale'] = 'TRUE' if  pc_job['total-color-pages'] else 'FALSE'
     return pc_job 
 
-def get_new_jobs(old_pc_jobs, pc_jobs):   
-    """Return dict of PaperCut jobs that in pc_jobs (latest jobs found 
-        on Fiery) but not in old_pc_jobs (Fiery jobs recored in 
-        PaperCut)
-        
-        old_pc_jobs: dict of Fiery jobs recorded in PaperCut
-        pc_jobs: dict of latest jobs found on Fiery
-        Returns: dict of jobs that are printed on Fiery but not yet recorded in PaperCut
-        (All params and return are dicts of jobs in PaperCut format)
-        
-        TODO: 
-            Check that this correctly reflects Fiery jobs with 
-             Fiery team.
-            Find out if there is a way of querying Fiery to return
-             only jobs printed since previous query.
-    """
-    old_keys = set(old_pc_jobs.keys()) 
-    new_jobs = {}
-    for id,job in pc_jobs.items():
-        if id not in old_keys:
-            new_jobs[id] = job
-        else:
-            old_job = old_pc_jobs[id]
-            if job['total-pages'] > old_job['total-pages']:
-                new_job = copy.copy(job)
-                new_job['total-pages'] = job['total-pages'] - old_job['total-pages']
-                new_job['total-color-pages'] = job['total-color-pages'] - old_job['total-color-pages']
-                new_jobs[id] = new_job
-    return new_jobs            
 
 #
 # Command line processing
@@ -269,6 +276,7 @@ DEFAULT_FIERY_API_KEY_FILE = 'fiery.api.key'
 DEFAULT_FIERY_IP = '192.68.228.104'   
 DEFAULT_FIERY_USER = 'admin' 
 DEFAULT_FIERY_PWD = 'Fiery.color' 
+DEFAULT_FIERY_BATCH_SIZE = 1000 
 
 DEFAULT_PAPERCUT_IP = 'localhost'
 DEFAULT_PAPERCUT_PORT = 9191
@@ -278,8 +286,10 @@ DEFAULT_PAPERCUT_ACCOUNT = 'Fiery-account'
 DEFAULT_SLEEP_SECS = 60
 
 parser = optparse.OptionParser('python %s [options]' % sys.argv[0])
+parser.add_option('-B', '--fiery-batch-size', dest='fiery_batch_size', default=DEFAULT_FIERY_BATCH_SIZE, 
+        help='Number of jobs to request in each call to Fiery')
 parser.add_option('-K', '--fiery-api-key', dest='fiery_api_key_file', default=DEFAULT_FIERY_API_KEY_FILE, 
-        help='Path of Fiery API key file')
+        help='Path of Fiery API key file')        
 parser.add_option('-S', '--fiery-ip', dest='fiery_ip', default=DEFAULT_FIERY_IP, 
         help='Network name or IP address of Fiery to monitor')
 parser.add_option('-U', '--fiery-user', dest='fiery_user', default=DEFAULT_FIERY_USER, 
@@ -297,7 +307,9 @@ parser.add_option('-a', '--papercut-account', dest='papercut_account', default=D
 parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False, 
         help='verbose output')     
 parser.add_option('-t', '--sleep-secs', dest='sleep_secs', type='int', default=DEFAULT_SLEEP_SECS, 
-        help='Sleep time between successive Fiery polls')        
+        help='Sleep time between successive Fiery polls')    
+parser.add_option('-i', '--ignore-history', action='store_true', dest='ignore_history', default=False, 
+        help='Ignore history of Fiery jobs logged on PaperCut and log all jobs again.')        
 
 #
 # Execution starts here
@@ -320,20 +332,31 @@ print('-' * 80)
 fy_api_key = fiery_load_api_key(options.fiery_api_key_file)
 if not fy_api_key:
     exit()
-print(options.fiery_api_key_file)
-print('Fiery API Key="%s"' % fy_api_key) 
+
+#if options.verbose:
+#    print(options.fiery_api_key_file)   
+#    print('Fiery API Key="%s"' % fy_api_key) 
 fy_url,fy_session_cookie = fiery_login(fy_api_key, options.fiery_ip, 
         options.fiery_user, options.fiery_pwd, options.verbose) 
 
 # Initialize PaperCut        
+pc_ip = options.papercut_ip
 pc_auth_token = options.papercut_pwd
 pc_account = options.papercut_account
 pc_server = papercut_init(options.papercut_ip, options.papercut_port,
         pc_auth_token, options.papercut_account)   
 
 # Load record of Fiery jobs already logged on PaperCut
-# TODO: Store this in PaperCut in some way        
-old_pc_jobs = load_object('fiery.papercut.old.jobs', {})         
+# TODO: Store this in PaperCut in some way  
+fy_last_ids = {} if options.ignore_history else load_object('fiery.last.ids', {}) 
+    
+if options.verbose:
+    print('Latest job ids from previous session: %s' % fy_last_ids) 
+fy_start_id = fy_last_ids.get(options.fiery_ip, 0)   
+
+fy_count = options.fiery_batch_size   
+
+print('=' * 80)    
 
 # Main loop
 #   Poll Fiery for list of printed jobs
@@ -343,30 +366,32 @@ old_pc_jobs = load_object('fiery.papercut.old.jobs', {})
 #       Log new jobs
 #       Update list record of jobs already logged
 while True:  
-
-    print('-' * 80)  
+ 
     # Fetch Fiery jobs
-    fy_jobs = fiery_fetch_jobs(fy_url, fy_session_cookie, options.verbose)
-    print('Fetched %d jobs from %s' % (len(fy_jobs), fy_url))
-    if options.verbose:
-        print(fy_print_jobs) 
-
-    # Convert Fiery jobs to PaperCut format
-    pc_jobs = dict((job['id'],job_fiery_to_papercut(job, pc_account)) for job in fy_jobs)
+    fy_jobs = fiery_fetch_jobs(fy_url, fy_session_cookie, fy_start_id, fy_count, options.verbose)
     
-    # Find out which of the Fiery jobs are new
-    new_pc_jobs = get_new_jobs(old_pc_jobs, pc_jobs)
-    if new_pc_jobs:
-        # Log new Fiery jobs on PaperCut 
-        print('%d new jobs' % len(new_pc_jobs))
+    if fy_jobs:
+        print('Fetched %d jobs from %s' % (len(fy_jobs), fy_url))
         if options.verbose:
-            print(new_pc_jobs) 
-        papercut_log_jobs(pc_server, pc_auth_token, pc_jobs)    
-        
+            print(fy_jobs) 
+
+        # Convert Fiery jobs to PaperCut format
+        pc_jobs = dict((job['id'],convert_job(job, pc_ip, pc_account)) for job in fy_jobs)
+        if options.verbose:
+            print(pc_jobs) 
+
+        # Log latest Fiery on PaperCut
+        papercut_log_jobs(pc_server, pc_auth_token, pc_jobs) 
+
         # Update record of jobs already logged
-        old_pc_jobs = pc_jobs
-        save_object('fiery.papercut.old.jobs', old_pc_jobs)   
+        max_id = max(pc_jobs.keys())
+        assert max_id >= fy_start_id, 'Highest job id=%d < start id=%d' % (max_id, fy_start_id)
+
+        fy_start_id = max_id + 1
+        fy_last_ids[options.fiery_ip] = fy_start_id   
+        save_object('fiery.last.ids', fy_last_ids)   
 
     if options.verbose:
         print('Sleeping %d sec' % options.sleep_secs)
+        print('-' * 80)  
     time.sleep(options.sleep_secs)
