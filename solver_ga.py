7a02e3fc2fa2619947a5f07be752b0ff10d6bb25
Peter Williams <peter.williams.97@gmail.com>
2013-07-02 17:42:44 +1000

    added knapsack

--- solver_ga.py
+++ solver_ga.py
@@ -2,64 +2,82 @@
 # -*- coding: utf-8 -*-
 """
     A genetic algorithm implementation
+    
+ -------
+    3966813
+    3967180   
+    
+http://doughellmann.com/2008/05/pymotw-heapq.html    
+    
+WEIGHT_RATIO = 0.8
+N_ENTRIES = 1000    
+
+best: 3951089 [3951089, 3476930, 3476521, 3476625, 3476020]
+best: 3951558 [3951558, 3476930, 3476521, 3476625, 3476020]
+best: 3958496 [3958496, 3476930, 3476521, 3476625, 3476020]
+best: 3958856 [3958856, 3476930, 3476521, 3476625, 3476020]
+best: 3959132 [3959132, 3476930, 3476521, 3476625, 3476020]
+best: 3959181 [3959181, 3476930, 3476521, 3476625, 3476020]
+best: 3959271 [3959271, 3476930, 3476521, 3476625, 3476020]
+best: 3959414 [3959414, 3476930, 3476521, 3476625, 3476020]
+best: 3960050 [3960050, 3476930, 3476521, 3476625, 3476020]
+best: 3960593 [3960593, 3476930, 3476521, 3476625, 3476020]
+best: 3960615 [3960615, 3476930, 3476521, 3476625, 3476020]
+best: 3962062 [3962062, 3476930, 3476521, 3476625, 3476020]
+best: 3963690 [3963690, 3476930, 3476521, 3476625, 3476020]
+     
+      
+--------------------------------------------------------------------------------
+
+WEIGHT_RATIO = 0.9
+N_ENTRIES = 2000
+
+best: 3951474 [3951474, 3480800, 3480664, 3480762, 3480607]
+best: 3952149 [3952149, 3480800, 3480664, 3480762, 3480607]
+best: 3954524 [3954524, 3480800, 3480664, 3480762, 3480607]
+best: 3956238 [3956238, 3480800, 3480664, 3480762, 3480607]
+best: 3957084 [3957084, 3480800, 3480664, 3480762, 3480607]
+best: 3957926 [3957926, 3480800, 3480664, 3480762, 3480607]
+best: 3958718 [3958718, 3480800, 3480664, 3480762, 3480607]
+best: 3958729 [3958729, 3480800, 3480664, 3480762, 3480607]
+best: 3958899 [3958899, 3480800, 3480664, 3480762, 3480607]
+best: 3960268 [3960268, 3480800, 3480664, 3480762, 3480607]
+best: 3960381 [3960381, 3480800, 3480664, 3480762, 3480607]
+best: 3961357 [3961357, 3480800, 3480664, 3480762, 3480607]
+best: 3961818 [3961818, 3480800, 3480664, 3480762, 3480607]
+best: 3961927 [3961927, 3480800, 3480664, 3480762, 3480607]
+best: 3962126 [3962126, 3480800, 3480664, 3480762, 3480607]
+best: 3962281 [3962281, 3480800, 3480664, 3480762, 3480607]
+best: 3962871 [3962871, 3480800, 3480664, 3480762, 3480607]
+ 
 
 """
 from __future__ import division
 import sys, os, random
-from math import *
+import numpy as np
 
-# Set random seed so that each run gives same results
-random.seed(555)
+n_elems = 0
 
-WEIGHT_RATIO = 0.8
+WEIGHT_RATIO = 0.9
+N_ENTRIES = 2000
 
-#
-# Based on roulette dicts
-#
-
-N_ENTRIES = 1000
-TOTAL_WEIGHT = sum([WEIGHT_RATIO**(i+1) for i in range(N_ENTRIES)]) 
-WEIGHT_SERIES = tuple([WEIGHT_RATIO**(i+1)/TOTAL_WEIGHT for i in range(N_ENTRIES)])
-
-def applyWeights(Q):
-    """roulette_in is a list of roulette dicts
-        Add and 'idx' and 'weight' keys to roulette dicts 
-        'weight' value is based on 'score' value of dict.
-        Return list of roulette dicts sorted by weight
-        For use in rouletteWheel 
-    """
-    for i, x in enumerate(Q):
-        x.weight = WEIGHT_SERIES[i]
+WEIGHTS = WEIGHT_RATIO ** (1 + np.arange(N_ENTRIES))
+WEIGHTS = np.cumsum(WEIGHTS)
+WEIGHTS /= WEIGHTS[-1]      # Cumulative sums up to 1.0
 
     
-def _spin_wheel(Q):
+def spin_roulette_wheel():
     """ Find the roulette wheel winner
-        returns: an index with probability proportional to dict's 'weight'
+        returns: an index with probability proportional to index's weight
     """
-    v = random.random()             # The value we threw
-    bottom = 0.0                    # Bottom value of the ith entry
-    for i, x in enumerate(Q):
-        top = bottom + x.weight     # Top value of the ith entry
-        if v <= top:
-            return i
-        base = top
-    raise RuntimeError('Cannot be here')
+    return np.searchsorted(WEIGHTS, random.random())
 
-def spinRouletteWheel(roulette_in):
-    """ Find the roulette wheel winner
-        roulette_in: a list of dicts with keys 'idx' and 'weight'
-        returns: an index with probability proportional to dict's 'weight'
-    """
-    roulette = applyWeights(roulette_in)
-    return _spin_wheel(roulette)
     
-    
-def spinRouletteWheelTwice(roulette):
+def spin_roulette_wheel_twice():
     """" Spin the roulette wheel twice and return 2 different values """
-    roulette = applyWeights(roulette)
-    i1 = _spin_wheel(roulette)
+    i1 = spin_roulette_wheel()
     while True:    
-        i2 = _spin_wheel(roulette)
+        i2 = spin_roulette_wheel()
         if i2 != i1:
             return i1, i2
 
@@ -68,68 +86,101 @@ N_REPLACEMENTS = 1
 def mutate(elements, complement):
     """Replace N_REPLACEMENTS of the in elements itmes from complement and return it"""
        
-    taken = elements[:]    
-    for _ in range(N_REPLACEMENTS):
-        taken[randint(0, len(taken)] = complement[randint(0, len(taken)]
-    return taken
+    #print 'mutate:', len(elements), len(complement) 
+    assert len(elements) + len(complement) == n_elems
+    added = random.sample(complement, N_REPLACEMENTS)
+    removed = random.sample(elements, N_REPLACEMENTS)
+    
+    return set(added), set(removed)
     
 
 def crossOver(c1, c2):
     """Swap half the elements in c1 and c2
-        c1: a list of ints
-        c2: a list of ints
+        c1: a set of ints
+        c2: a set of ints
     """
 
     # Find elements that are not in both lists
-    d1 = sorted(c1, key = lambda x: x in c2)
-    d2 = sorted(c2, key = lambda x: x in c1)
-    for i1, x in enumerate(d1):
-        if x in d2:
-            break
-    for i2, x in enumerate(d2):
-        if x in d1:
-            break
-    m = min(i1, i2)     # number of non-shared elements
-
-    shared = d1[m:]     # elements in both lists
-    d1 = d1[:m]         # elements only in c1
-    d2 = d2[:m]         # elements only in c2    
-    shuffle(d1)
-    shuffle(d2)
-    m1 = len(d1) // 2
-    m2 = len(d2) // 2
-    
+    shared = c1 &  c2
+    d1 = c1 - shared
+    d2 = c2 - shared
+   
+    move_1_to_2 = random.sample(d1, (len(d1) + 1)// 2)
+    move_2_to_1 = random.sample(d2, (len(d2) + 1)// 2)
+        
     # Return shared + 1/2 of original + 1/2 swapped
-    return shared + d1[:m1] + d2[m2:], shared + d2[:m2] + d1[m1:] 
-    
- 
-    
-def mutate(n, capacity, values_weights, elements):
-    complement = list(set(xrange(n)) - set(elements))
-    elements = _mutate(elements, complement)
-        
-    weight = sum(values_weights[i][1] for i in elements)
-    while weight > capacity:
-        i = elements.pop()
-        weight -= values_weights[i][1]
-    
-    return elements    
-        
-        
-def get_score(capacity, values_weights, elements):
+    return set(move_2_to_1), set(move_1_to_2)  
+
+  
+def get_state(values_weights, capacity, elements):  
     value = sum(value_weight[i][0] for i in elements)
     weight = sum(value_weight[i][1] for i in elements)
-    if weight <= capacity:
-        return weight - capacity, value
+    return value, capacity - weight
+
+    
+def update_state(values_weights, value, capacity, added, removed):
+    value += sum(values_weights[i][0] for i in added) - sum(values_weights[i][0] for i in removed)
+    capacity -= sum(values_weights[i][1] for i in added) - sum(values_weights[i][1] for i in removed)   
+    return value, capacity    
+    
+    
+def get_score(value, capacity):
+    if capacity >= 0:
+        return True, value
     else: 
-        return None, value / (weight / capacity) / 2.0    
+        return False, value / (1.0 - capacity) / 2.0    
     
 class Solution:
-    """A solution to the knapsack problem"""
+    """A solution to the knapsack problem
+        capacity: remaining capacity in the knapscack
+        value: value of items in the knapscak
+        elements: elements included in the knapsack
+        complement: elements not included in the knapsacl
+    """
 
-    def __init__(self, capacity, values_weights, elements):
+    def __init__(self, value, capacity, elements, complement):
+        self.capacity = capacity
+        self.value = value
         self.elements = elements
-        self.score = get_score(capacity, values_weights, elements)
+        self.complement = complement
+        assert len(elements) + len(complement) == n_elems
+        assert not self.elements & self.complement
+        
+    def score(self):
+        valid, scor = get_score(self.value, self.capacity)
+        if valid:
+            assert scor == self.value, 'valid=%s,score=%s,value=%s' % (valid, scor, self.value)
+        return scor
+        
+    def update(self, values_weights, added, removed):
+        value, capacity = update_state(values_weights, self.value, self.capacity, added, removed)
+        elements = (self.elements | added) - removed
+        complement = (self.complement | removed) - added
+        assert not self.elements & self.complement
+        assert not added & removed
+        assert len(elements) + len(complement) == n_elems, '\n%d %s\n%d %s\n%s\n%s\n%s' % (
+                len(elements), sorted(elements), 
+                len(complement), sorted(complement),
+                elements & complement,
+                added,
+                removed,
+               )
+        return Solution(value, capacity, elements, complement) 
+        
+    def top_up(self, values_weights):
+        if self.capacity <= 0:
+            return
+        for i, (v, w) in enumerate(values_weights):
+            if i in self.complement and w <= self.capacity:
+                self.value += v
+                self.capacity -= w
+                self.elements.add(i)
+                self.complement.remove(i)
+                
+    def update_top_up(self, values_weights, added, removed):            
+        solution = self.update(values_weights, added, removed)
+        solution.top_up(values_weights)
+        return solution
         
         
 def solve_greedy(capacity, value, values_weights, elements, complement):
@@ -151,10 +202,14 @@ def solve_greedy(capacity, value, values_weights, elements, complement):
             break
         value += v
         capacity -= w
-        elements.append(i)
+        taken.append(i)
 
-    return value, elements        
+    return value, capacity, taken        
     
+    
+from itertools import count
+from heapq import heappush, heappop, heappushpop    
+
 INVERSE_MUTATION_RATIO = 10    
 def solve_ga(capacity, values, weights):
     
@@ -162,36 +217,63 @@ def solve_ga(capacity, values, weights):
     
     values_weights = zip(values, weights)
     n = len(values_weights)
+    
+    global n_elems
+    n_elems = n
+    
+    print 'WEIGHT_RATIO',  WEIGHT_RATIO
+    print 'N_ENTRIES', N_ENTRIES 
+    
     best_value = 0
     
     # Prime the heap with 1000 elements
-    elements, complement = [], range(N)
+    elements, complement = [], range(n)
+    complem_set = set(complement)
     Q = []
-    for _ in range(1000):
-        value, taken = solve_greedy(capacity, 0, values_weights, elements, complement)
-        entry = Solution(value, taken)
-        heappush(Q, (entry.score, entry))
-        shuffle(complement)   
+    for i in range(N_ENTRIES * 10):
+        value, cap, elts = solve_greedy(capacity, 0, values_weights, elements, complement)
+        elts = set(elts)
+        #if i % N_ENTRIES == N_ENTRIES - 1:
+        #    print (-Q[0][0], value, -value/Q[0][0],  i), 
+        assert len(elts) and len(complem_set - elts), '%d %d' % (len(elts), len(complem_set - elts))     
+        solution = Solution(value, cap, elts, complem_set - elts)
+        assert len(solution.elements) and len(solution.complement), '%d %d' % (len(solution.elements), len(solution.complement)) 
+        if len(Q) < N_ENTRIES:
+            heappush(Q, (solution.score(), solution))
+        else: 
+            heappushpop(Q, (solution.score(), solution))
+        random.shuffle(complement) 
 
+    best_value = Q[-1][0]
+    print
+    print 'best:', best_value, [int(Q[-i][1].score()) for i in range(1,6)], Q[0][1].score()        
+    print '-' * 80
+    
+    counter = count()
+ 
     while True:
         if next(counter) % INVERSE_MUTATION_RATIO == 0:
-            i = spinRouletteWheel(Q)
-            elements, complement = mutate(Q[i].elements, Q[i].complement)
-            cap, value = get_score(capacity, values_weights, elements):
-            if cap:
-                value, taken = solve_greedy(cap, 0, values_weights, elements, complement)
-            e = Solution(value, elements)    
-            heapreplace(Q, (e.score, e)    
+            i = spin_roulette_wheel()
+            added, removed = mutate(Q[i][1].elements, Q[i][1].complement)
+            solution = Q[i][1].update_top_up(values_weights, added, removed)
+            heappushpop(Q, (solution.score(), solution))    
         else:    
-            i1, i2 = spinRouletteWheelTwice(Q)
-            c1, c2 = crossOver(Q[i1].elements, Q[i2].elements)
-            for c in c1, c2:
-                cap, value = get_score(capacity, values_weights, elements):
-                if cap:
-                    value, taken = solve_greedy(cap, 0, values_weights, elements, complement)
-                e = Solution(value, elements)    
-                heapreplace(Q, (e.score, e)    
+            i1, i2 = spin_roulette_wheel_twice()
+            move_2_to_1, move_1_to_2 = crossOver(Q[i1][1].elements, Q[i2][1].elements)
+            for i, added, removed in (i1, move_2_to_1, move_1_to_2), (i2, move_1_to_2, move_2_to_1):
+                solution = Q[i][1].update_top_up(values_weights, added, removed)
+                heappushpop(Q, (solution.score(), solution))    
 
-        if Q[0].score > best_value:
-            print best_value
+        if Q[-1][1].score() > best_value:
+            best_value = Q[-1][1].score()
+            print 'best:', best_value, [int(Q[-i][1].score()) for i in range(1,6)], Q[0][1].score()  
+            path = 'best%d.results' % best_value
+            with open(path, 'rt') as f:
+                f.write('WEIGHT_RATIO=%d\n' %  WEIGHT_RATIO)
+                f.write('N_ENTRIES=%d\n' %  N_ENTRIES)
+                for i in range(1,6):
+                    q = Q[-i][1]
+                    els = ', '.join('%d' % j for j in sorted(q.elements))
+                    f.write('%d: %s\n' % (q.score(), els)) 
+            
 
---
+++ solver_h3.py
@@ -0,0 +1,242 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+from fractions import gcd
+import time
+import logging
+from solver_ga import solve_ga
+
+def solve_greedy(capacity, values, weights):
+
+    items = len(values)
+    # a trivial greedy algorithm for filling the knapsack
+    # it takes items in-order until the knapsack is full
+    value = 0
+    weight = 0
+    taken = []
+
+    for i in range(0, items):
+        if weight + weights[i] <= capacity:
+            taken.append(1)
+            value += values[i]
+            weight += weights[i]
+        else:
+            taken.append(0)
+
+    return value, taken
+
+
+def solve_dp(capacity, values, weights):
+    """Build a dynamic programming table with each column is a capacity
+        and each row the number of elements
+    """
+    
+    logging.info('solve_dp')
+        
+    vw = zip(values, weights)
+    n_items = len(vw)
+    table = [[0] * (capacity+1) for _ in range(n_items+1)]
+  
+    for i in range(1, n_items + 1):
+        v, w = vw[i-1]
+        for cap in range(1, capacity + 1):
+            if w > cap:
+                table[i][cap] = table[i-1][cap]
+            else:
+                candidate1 = table[i-1][cap]
+                candidate2 = table[i-1][cap-w] + v 
+                table[i][cap] = max(candidate1, candidate2)
+    
+    value = table[-1][-1]            
+    weight = capacity
+    taken = []
+    for i in range(n_items, 0, -1):
+        if table[i][weight] != table[i-1][weight]:
+            taken.append(i-1)
+            weight -= vw[i-1][1]
+            if weight == 0:
+                break
+                
+    return value, taken, True
+
+    
+from itertools import count
+from heapq import heappush, heappop
+
+DEPTH_FIRST, BEST_FIRST, HYBRID = range(3)
+
+def solve_bb(capacity, values, weights, method, max_time):
+    """Branch and bound solution"""
+    
+    logging.info('solve_bb" method=%d,max_time=%d', method, max_time)
+    
+    n = len(values)
+    indexes = range(n)
+    indexes.sort(key=lambda i: -values[i]/weights[i])
+    values_weights = [(values[i], weights[i]) for i in indexes]
+ 
+    best = [0, []]
+    
+    def ancestors(ptr):
+        ancests = []
+        while ptr:
+            ptr, item = ptr
+            ancests.append(item)
+        return ancests
+
+    counter = count()
+    end_time = time.time() + max_time
+    timedout = [False]
+    
+    def times_up(current_count):
+        if not current_count % 1000:
+            return False
+        return time.time() > end_time
+
+    def bound(sv, sw, m):
+        """Return an upper bound on the value of a knapsack
+            whose first m items (in v/w sorted order) have
+            value sv and weight sw
+        """
+        if m == n:
+            return sv
+        for av, aw in values_weights[m:]:
+            if sw + aw > capacity:
+                break
+            sv += av
+            sw += aw
+        return sv + (capacity - sw) * av/aw
+    
+    def branch(sv, sw, m, parent):
+        """Given a node n elements from the root with 
+            value sv and weight sw in the m elements,
+            generate all nodes below this node
+        """
+        if sw > capacity:
+            return
+        if sv > best[0]:
+            best[0], best[1] = sv, parent
+            print 'best=%d' % best[0],
+            if best[0] > 8000:
+                print sorted(ancestors(parent))
+            else:
+                print
+        if m == n:
+            return
+        i = indexes[m]
+        v, w = values_weights[m]
+        choices = ((sv, sw, parent), (sv + v, sw + w, [parent, i]))
+        for sv, sw, ptr in choices:
+            b = bound(sv, sw, m+1)
+            if b > best[0]:
+                yield b, branch(sv, sw, m+1, ptr)
+
+
+       
+    def explore_depth_first(node):
+        print 'number items:', len(values_weights), len(values), len(weights)
+        stack = [node]
+        while stack and not timedout[0]:
+            for _, node in stack.pop():
+                stack.append(node)
+                
+    def explore_best_first(node):            
+        heap = [(0, next(counter), node)]
+        while heap and not timedout[0]:
+            for b, node in heappop(heap)[2]:
+                heappush(heap, (b, next(counter), node))
+                
+    MAX_ENTRIES = 1000 * 1000 * 10           
+
+    def explore_hybrid(node):            
+        heap = [(0, next(counter), node)]
+        while heap and not timedout[0]:
+            _, _, results = heappop(heap)
+            for b, node in results:
+                #print (len(heap),MAX_ENTRIES),
+                if len(heap) > MAX_ENTRIES:
+                    print 'depth-first'
+                    explore_depth_first(node)
+                else:
+                    heappush(heap, (b, next(counter), node))            
+
+    if method == DEPTH_FIRST:
+        explore_best_first(branch(0, 0, 0, None))
+    elif method == BEST_FIRST:
+        explore_depth_first(branch(0, 0, 0, None))
+    elif method == HYBRID:
+        explore_hybrid(branch(0, 0, 0, None))
+        
+    return best[0], ancestors(best[1]), not timedout[0]
+    
+
+def gcds(lst):
+    a = lst[0]
+    for b in lst[1:]:
+        a = gcd(a, b)
+    return a
+
+    
+def solve(capacity, values, weights):
+
+    MAX_TIME = 4 * 60 * 60
+
+    d = gcds(weights)
+    if d > 1:
+        weights = [w//d for w in weights]
+        capacity = capacity//d
+
+    n = len(values)
+    
+    print 'n=%d' % n
+    print 'capacity=%d' % capacity
+    print 'n * capacity=%d' % (n * capacity)
+        
+    # !@#$    
+    value, taken, optimal = solve_ga(capacity, values, weights)    
+        
+    if n < 20 or n * capacity > 10 ** 8:
+        value, taken, optimal = solve_bb(capacity, values, weights, HYBRID, MAX_TIME)
+    else: 
+        value, taken, optimal = solve_dp(capacity, values, weights)
+    
+    taken = set(taken)
+    return value, [1 if i in taken else 0 for i in range(n)], optimal
+
+    
+def solveIt(inputData):
+    # Modify this code to run your optimization algorithm
+
+    # parse the input
+    lines = inputData.split('\n')
+
+    firstLine = lines[0].split()
+    items = int(firstLine[0])
+    capacity = int(firstLine[1])
+
+    values = []
+    weights = []
+
+    for i in range(1, items + 1):
+        line = lines[i]
+        parts = line.split()
+        values.append(int(parts[0]))
+        weights.append(int(parts[1]))
+
+    value, taken, optimal = solve(capacity, values, weights)
+
+    # prepare the solution in the specified output format
+    return '%d %d\n%s' % (value, int(optimal), ' '.join(map(str, taken)))
+
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        fileLocation = sys.argv[1].strip()
+        inputDataFile = open(fileLocation, 'r')
+        inputData = ''.join(inputDataFile.readlines())
+        inputDataFile.close()
+        print solveIt(inputData)
+    else:
+        print 'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)'
+
